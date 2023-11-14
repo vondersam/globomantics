851e653979a9d0c36546835a41d3003ed795a075
Samuel Rodriguez <samuelyret@gmail.com>
2023-11-14 14:08:51 +0100

    Add conditional class name.

--- components/houseRow.js
+++ components/houseRow.js
@@ -5,7 +5,11 @@ const HouseRow = ({ house }) => {
     <tr>
       <td>{house.address}</td>
       <td>{house.country}</td>
-      <td>{currencyFormatter.format(house.price)}</td>
+      {house.price && (
+        <td className={house.price >= 500000 ? 'text-primary' : ''}>
+          {currencyFormatter.format(house.price)}
+        </td>
+      )}
     </tr>
   );
 };
