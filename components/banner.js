fa2c74ec661a059f3562d53bac2f7d1db4102bb6
Samuel Rodriguez <samuelyret@gmail.com>
2023-12-16 12:56:29 +0100

    Add navigation to home.

--- components/banner.js
+++ components/banner.js
@@ -1,8 +1,18 @@
+import { useContext } from 'react';
+import { navigationContext } from './app';
+import navValues from '@/helpers/navValues';
+
 const Banner = ({ children }) => {
+  const { navigate } = useContext(navigationContext);
   return (
     <header className="row mb-4">
       <div className="col-5">
-        <img src="/./GloboLogo.png" alt="logo" className="logo" />
+        <img
+          src="/./GloboLogo.png"
+          alt="logo"
+          className="logo"
+          onClick={() => navigate(navValues.home)}
+        />
       </div>
       <div className="col-7 mt-5 themeFontColor">{children}</div>
     </header>
